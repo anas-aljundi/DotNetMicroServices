4092dc14cfb74f5248266edde3db356109f73c76
Anas-Aljundi <anas.algendi@gmail.com>
2023-02-28 22:13:45 +0400

    Domain Layer

---
+++ src/Services/Ordering/Ordering.Domain/Common/EntityBase.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.Domain.Common
+{
+    public abstract class EntityBase
+    {
+        public int Id { get; protected set; }
+        public string CreatedUser { get; set; }
+        public DateTime CreateDate { get; set; }
+        public string UpdateUser { get; set; }
+        public DateTime UpdateDate { get; set; }
+        public bool IsDeleted { get; set; } = false;
+        public string DeleteUser { get; set; }
+        public DateTime? DeleteDate { get; set; }
+    }
+}
